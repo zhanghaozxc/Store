1a39da1fa4559a21ddaf838708f803ce6550f392
haozhang <haozhang>
2019-09-24 16:08:46 -0400

    [New] UnitTest

---
+++ Store.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Store.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Store.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("320bf053-0383-4225-ab95-db2cbd6380cd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Store.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Store.Domain;
+using Store.Domain.Abstract;
+
+namespace Store.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepos repos;
+
+        public ProductController(IProductsRepos productsRepos)
+        {
+            this.repos = productsRepos;
+        }
+    }
+}
