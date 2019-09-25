94118c7253c59982eb868e5ffd5219b9ba0f34aa
haozhang <haozhang>
2019-09-25 10:47:20 -0400

    [New] add InMemoryProductsRepos

---
+++ Store.Domain/Concrete/InMemoryProductsRepos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Store.Domain.Abstract;
+using Store.Domain;
+
+namespace Store.Domain.Concrete
+{
+    public class InMemoryProductsRepos : IProductsRepos
+    {
+        public IEnumerable<Product> Products
+        {
+            get
+            {
+                return new List<Product>
+                {
+                    new Product { Name = "Football", Price = 25 },
+                    new Product { Name = "Cake", Price = 12},
+                    new Product { Name = "Orange", Price = 1.99M},
+                };
+            }
+        }
+
+    }
+}
--- Store.WebApp/App_Start/IocConfig.cs
+++ Store.WebApp/App_Start/IocConfig.cs
@@ -10,16 +10,6 @@ namespace Store.WebApp.App_Start
 {
     public class IocConfig
     {
-        private void AddBindings()
-        {
-            Mock<IProductsRepos> mock = new Mock<IProductsRepos>();
-            mock.Setup(m => m.Products).Returns(new List<Product>
-            {
-                new Product { Name = "Football", Price = 25 },
-                new Product { Name = "Surf board", Price = 179 },
-                new Product { Name = "Running shoes", Price = 95 }
-            });
-            kernel.Bind<IProductsRepos>().ToConstant(mock.Object);
-           }
+
     }
 }
