f4358d914ae2e4d3c0a740df3b49a1d8a6ca01c1
haozhang <haozhang>
2019-09-26 15:42:35 -0400

    [New] add PagingHelpers and ProductsListViewModel

---
+++ Store.WebApp/Models/ProductsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Store.WebApp.Models
+{
+    public class ProductsListViewModel
+    {
+    }
+}
